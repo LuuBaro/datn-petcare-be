c956266a4b1a456c016df634150e68aeb0c66242
HMinh04 <164726487+HMinh04@users.noreply.github.com>
2025-01-19 15:55:19 +0700

    model Product, Favorites

---
+++ petcare-be/src/main/java/org/example/petcarebe/model/Favorites.java
@@ -0,0 +1,25 @@
+package org.example.petcarebe.model;
+
+import com.restfb.types.whatsapp.platform.send.interactive.Section;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "favorites")
+public class Favorites {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long favoritesId;
+    private Date like_date;
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "productId")
+    private Products products;
+
+}
