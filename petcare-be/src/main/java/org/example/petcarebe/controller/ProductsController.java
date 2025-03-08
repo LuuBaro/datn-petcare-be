478fc321e29470c5ac75f4682171592c33f6a5c8
ThanhPhatFPT <156346544+ThanhPhatFPT@users.noreply.github.com>
2025-03-09 01:53:20 +0700

    fix

--- petcare-be/src/main/java/org/example/petcarebe/controller/ProductsController.java
+++ petcare-be/src/main/java/org/example/petcarebe/controller/ProductsController.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "*") // Cho phép truy cập từ frontend
 public class ProductsController {
 
     @Autowired
